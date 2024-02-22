5e530734f2803f29fb4dee7a734e83fcfe456fc8
Duy Nguyen <duytnguyendtn.open@gmail.com>
2024-02-22 10:12:24 -0500

    Sanitize subproc output before translating to JSON

--- jpyjs9/js9.py
+++ jpyjs9/js9.py
@@ -62,13 +62,16 @@ class JS9(JS9_):
         # Derive URL subfolder path from Jupyter URL:
         logging.debug("Evaluating default frame url...")
         try:
-            jupyter_base_path = Path(json.loads(subprocess.check_output(["jupyter", "lab", "list", "--json"]))['base_url'])
+            subproc_out = subprocess.check_output(["jupyter", "lab", "list", "--json"])
+            # Extract out only the json element by finding the first '{' and last '}'
+            current_jupyter_session = subproc_out[subproc_out.index('{'):subproc_out.rfind('}')+1]
+            jupyter_base_path = Path(json.loads(current_jupyter_session)['base_url'])
             frame_url_fallback = (jupyter_base_path / "js9").as_posix()
             logging.debug("Successfully retrieved Jupyter base_url path")
-        except:
+        except Exception as e:
             # Use our usual fallback frame url
             frame_url_fallback = "/js9"
-            logging.debug("Failed to retrieve Jupyter URL, using default fallback")
+            logging.debug(f"Failed to retrieve Jupyter URL: {str(e)}, using default fallback")
         frame_url = kwargs.get('frame_url', frame_url_fallback)
         logging.debug(f"Using frame URL: {frame_url}")
 
